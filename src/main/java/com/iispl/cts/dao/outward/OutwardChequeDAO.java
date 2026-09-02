c9eb84685381bb6e51f8569d1aebe9171e2e5c7c
Manya Shankar <manyasoumya007@gmail.com>
2026-09-02 11:34:02 +0530

    resolved conflicts

--- src/main/java/com/iispl/cts/dao/outward/OutwardChequeDAO.java
+++ src/main/java/com/iispl/cts/dao/outward/OutwardChequeDAO.java
@@ -1,19 +1,15 @@
 package com.iispl.cts.dao.outward;
 
-
 import java.math.BigDecimal;
-
 import java.util.List;
 
 import com.iispl.cts.entity.outward.OutwardCheque;
-public interface OutwardChequeDAO{
-	List<OutwardCheque> getChequesByBatchId(String outwardBatchId);	
 
+public interface OutwardChequeDAO {
 
-	List<OutwardCheque> getChequesByBatchId(String outwardBatchId);
+    List<OutwardCheque> getChequesByBatchId(String outwardBatchId);
 
-	int getTotalChequeCountByBatchId(String outwardBatchId);
+    int getTotalChequeCountByBatchId(String outwardBatchId);
 
-	BigDecimal getTotalChequeAmountByBatchId(String outwardBatchId);
+    BigDecimal getTotalChequeAmountByBatchId(String outwardBatchId);
 }
-
