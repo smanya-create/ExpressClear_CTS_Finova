019ad76214f1eecaf44999bd99fa7a288713e921
Manya Shankar <manyasoumya007@gmail.com>
2026-09-08 18:22:28 +0530

    checker routed

--- src/main/java/com/iispl/cts/controller/common/SidebarController.java
+++ src/main/java/com/iispl/cts/controller/common/SidebarController.java
@@ -231,7 +231,7 @@ public class SidebarController extends GenericForwardComposer<Component> {
 	}
 
 	public void navToOutwardCheckerUnprocessedQueue() {
-		Executions.sendRedirect("outward/checker/checker-unprocessed-cheques.zul");
+		Executions.sendRedirect("/outward/checker/checker-unprocessed-cheques.zul");
 	}
 
 	// Inward Maker Navigation Actions
