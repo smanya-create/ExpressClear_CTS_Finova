6af285182599357ad89eed41c8e72ea2e5b68222
SudhansuSekharPanda <sudhansusekharpanda000@gmail.com>
2026-09-15 17:17:09 +0530

    fixed batch coming in DE

--- src/main/java/com/iispl/cts/controller/inward/maker/InwardDataEntryBatchesController.java
+++ src/main/java/com/iispl/cts/controller/inward/maker/InwardDataEntryBatchesController.java
@@ -214,6 +214,11 @@ public class InwardDataEntryBatchesController extends GenericForwardComposer<Com
                      "JOIN inward_cheque c ON b.inward_batch_id = c.inward_batch_id " +
                      "WHERE b.batch_status NOT IN ('CHECKER_PROCESSING_PENDING', 'COMPLETED', 'REJECTED') " +
                      "GROUP BY b.inward_batch_id, b.actual_cheque_count, b.actual_total_amount, b.batch_status " +
+                     "HAVING COUNT(CASE WHEN c.cheque_status IN ('" 
+                     + InwardChequeStatus.DATA_ENTRY_PENDING.name() + "', '" 
+                     + InwardChequeStatus.DATA_ENTRY_IN_PROGRESS.name() + "', '" 
+                     + InwardChequeStatus.SEND_BACK_TO_MAKER_DATA_ENTRY.name() + "', '" 
+                     + InwardChequeStatus.SEND_BACK_TO_MAKER.name() + "') THEN 1 END) > 0 " +
                      "ORDER BY b.inward_batch_id ASC";
 
         try (Connection conn = DBConnection.getConnection();
