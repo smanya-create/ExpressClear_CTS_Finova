7b560ec051bd328bec81edaf1bf43f4a575e09aa
sanath <sanathpunnam417@gmail.com>
2026-09-17 10:38:50 +0530

    header commit

--- src/main/java/com/iispl/cts/controller/inward/checker/InwardCheckerReportsController.java
+++ src/main/java/com/iispl/cts/controller/inward/checker/InwardCheckerReportsController.java
@@ -243,13 +243,11 @@ public class InwardCheckerReportsController extends GenericForwardComposer<Compo
             return;
         }
 
-        String generatedBy = String.valueOf(
-                Sessions.getCurrent().getAttribute("LOGGED_USER"));
+    
 
         String xml = ReportXmlGenerator.generateRrfXml(
                 batchId,
-                rejectedCheques,
-                generatedBy);
+                rejectedCheques);
 
         Filedownload.save(
                 xml.getBytes(StandardCharsets.UTF_8),
@@ -277,8 +275,7 @@ public class InwardCheckerReportsController extends GenericForwardComposer<Compo
 
         String xml = ReportXmlGenerator.generateBatchSummaryXml(
                 batchId,
-                batchCheques,
-                generatedBy);
+                batchCheques);
 
         Filedownload.save(
                 xml.getBytes(StandardCharsets.UTF_8),
