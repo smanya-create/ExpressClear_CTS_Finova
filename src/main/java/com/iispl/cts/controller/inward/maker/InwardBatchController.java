5135d5a5eac43bf3f318fb2ce7f6af003346ca26
SHAIR YAAR KHAN <shairyaark932@gmail.com>
2026-09-24 15:43:27 +0530

    ccccc

--- src/main/java/com/iispl/cts/controller/inward/maker/InwardBatchController.java
+++ src/main/java/com/iispl/cts/controller/inward/maker/InwardBatchController.java
@@ -280,7 +280,7 @@ public class InwardBatchController extends SelectorComposer<Window> {
 	}
 
 	private String formatStatus(String status) {
-		String[] words = status.replace('_', ' ').trim().toLowerCase().split("\\\s+");
+		String[] words = status.replace('_', ' ').trim().toLowerCase().split("\\s+");
 		StringBuilder result = new StringBuilder();
 		for (String word : words) {
 			if (word.isEmpty())
